555f92acf508e31caa106a5eab1f9e4196a992e8
ARUN <arun.sekar@solitontech.com>
2026-08-25 15:25:19 +0530

    feat: Initial Commit

---
+++ Assessment/Project/View/TaskView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApplication.Services;
+
+namespace ToDoApplication.View
+{
+    internal class TaskView
+    {
+        private TaskSevices taskService;
+
+        public TaskView(TaskSevices taskService)
+        {
+            this.taskService = taskService;
+        }
+    }
+}
